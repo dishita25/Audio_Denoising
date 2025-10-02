6cb8b4089ca30cd37be4b1f48569b91659ff8c1f
Dishita Goyal <dishitagoyal25@gmail.com>
2025-10-02 09:44:20 +0530

    Changes in train.py

--- train.py
+++ train.py
@@ -1,49 +1,8 @@
-# import torch
-# from torch.utils.data import Dataset, DataLoader
-# from src.dataloader import *
-# from loss import wsdr_fn
-# from src.model.dcunet import DCUnet20
-# from trainer import train
-# import gc
-# from src.device import *
-
-# SAMPLE_RATE = 48000
-# N_FFT = (SAMPLE_RATE * 64) // 1000 
-# HOP_LENGTH = (SAMPLE_RATE * 16) // 1000 
-
-# train_input_files = sorted(list(TRAIN_INPUT_DIR.rglob('*.wav')))
-# train_target_files = sorted(list(TRAIN_TARGET_DIR.rglob('*.wav')))
-
-# test_noisy_files = sorted(list(TEST_NOISY_DIR.rglob('*.wav')))
-# test_clean_files = sorted(list(TEST_CLEAN_DIR.rglob('*.wav')))
-
-# print("No. of Training files:",len(train_input_files))
-# print("No. of Testing files:",len(test_noisy_files))
-
-# test_dataset = SpeechDataset(test_noisy_files, test_clean_files, N_FFT, HOP_LENGTH)
-# train_dataset = SpeechDataset(train_input_files, train_target_files, N_FFT, HOP_LENGTH)
-
-# test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
-# train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-
-# # For testing purpose
-# test_loader_single_unshuffled = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-# gc.collect()
-# torch.cuda.empty_cache()
-
-# dcunet20 = DCUnet20(N_FFT, HOP_LENGTH).to(DEVICE)
-# optimizer = torch.optim.Adam(dcunet20.parameters())
-# loss_fn = wsdr_fn
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
-
-# train_losses, test_losses = train(dcunet20, train_loader, test_loader, loss_fn, optimizer, scheduler, 4)
-
 import torch
 from torch.utils.data import Dataset, DataLoader
 from src.dataloader import *
-from loss import wsdr_fn
-from src.model.dcunet import DCUnet20
+from loss import zsn2n_loss_func
+from src.model.ZSN2N import network
 from trainer import train
 import gc
 from src.device import *
@@ -99,9 +58,9 @@ print("=" * 40)
 gc.collect()
 torch.cuda.empty_cache()
 
-dcunet20 = DCUnet20(N_FFT, HOP_LENGTH).to(DEVICE)
-optimizer = torch.optim.Adam(dcunet20.parameters())
-loss_fn = wsdr_fn
+zsn2n = network(N_FFT, HOP_LENGTH).to(DEVICE)
+optimizer = torch.optim.Adam(network.parameters())
+loss_fn = zsn2n_loss_func
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
 
-train_losses, test_losses = train(dcunet20, train_loader, test_loader, loss_fn, optimizer, scheduler, 4)
+train_losses, test_losses = train(network, train_loader, test_loader, loss_fn, optimizer, scheduler, 4)
