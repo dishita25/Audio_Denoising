5151c97d82aae75d5455d2c830f05d6c33f53a7d
MSarayu512 <sarayu.nasa@gmail.com>
2025-10-02 09:35:49 +0530

    pair_downsampler added

--- loss.py
+++ loss.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 from src.device import DEVICE
 from metrics import AudioMetrics2
+import torch.nn.functional as F
 
 # Repeating at a lot of places
 SAMPLE_RATE = 48000
@@ -140,3 +141,19 @@ def zsn2n_loss_func(noisy_stft, model):
     loss_cons = 0.5 * (mse(pred1, denoised1) + mse(pred2, denoised2))
 
     return loss_res + loss_cons
+
+
+def pair_downsampler(img):
+    #img has shape B C H W
+    c = img.shape[1]
+
+    filter1 = torch.FloatTensor([[[[0 ,0.5],[0.5, 0]]]]).to(img.device)
+    filter1 = filter1.repeat(c,1, 1, 1)
+
+    filter2 = torch.FloatTensor([[[[0.5 ,0],[0, 0.5]]]]).to(img.device)
+    filter2 = filter2.repeat(c,1, 1, 1)
+
+    output1 = F.conv2d(img, filter1, stride=2, groups=c)
+    output2 = F.conv2d(img, filter2, stride=2, groups=c)
+
+    return output1, output2
