81c9e7cba01c5db0ecf6c3f8d14f62a693ea89de
Dishita Goyal <dishitagoyal25@gmail.com>
2025-10-03 19:28:52 +0530

    Adding another conv layer

--- src/model/ZSN2N.py
+++ src/model/ZSN2N.py
@@ -17,11 +17,13 @@ class network(nn.Module):
 
         self.conv1 = nn.Conv2d(n_chan, chan_embed, 3, padding=1)
         self.conv2 = nn.Conv2d(chan_embed, chan_embed, 3, padding=1)
+        self.conv3 = nn.Conv2d(chan_embed, chan_embed, 3, padding=1)
 
-        self.conv3 = nn.Conv2d(chan_embed, n_chan, 1)
+        self.conv4 = nn.Conv2d(chan_embed, n_chan, 1)
 
     def forward(self, x):
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
-        x = self.conv3(x)
+        x = self.act(self.conv3(x))
+        x = self.conv4(x)
         return x
